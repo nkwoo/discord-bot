378471045ac98276b40521b467f9c88c1ed15b29
NKWOO <nkw9986@gmail.com>
2020-03-08 02:18:40 +0900

    [nkw] 1.5.8 2020-03-08
    1. 코로나 파싱 URL 변경 (정부 -> 네이버)

--- src/module/tool/corona.js
+++ src/module/tool/corona.js
@@ -1,8 +1,8 @@
-const maplePersonSearchUrl = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=&brdGubun=&ncvContSeq=&contSeq=&board_id=&gubun=";
+const coronaSearchUrl = "https://m.search.naver.com/search.naver?sm=mtp_hty.top&where=m&query=%EC%BD%94%EB%A1%9C%EB%82%9819";
 
-export function searchCoronaData (message, htmlparser, timeString) {
+export function searchCoronaData (message, htmlparser) {
     message.channel.send("데이터 조회중......").then((editMsg)=> {
-        htmlparser.getHtmlDocument(maplePersonSearchUrl).then(html => {
+        htmlparser.getHtmlDocument(coronaSearchUrl).then(html => {
 
             if (!html) {
                 editMsg.edit("데이터 조회 실패 ❌");
@@ -13,10 +13,17 @@ export function searchCoronaData (message, htmlparser, timeString) {
             const $ = htmlparser.changeHtmlToDom(html.data);
             let coronaArray = [];
 
-            $(".bvc_txt > .data_table > .num tr").each(function () {
-                let titleText = $(this).children("th").text();
-                let dataText = $(this).children("td").text();
-                coronaArray.push({name: titleText, value: dataText});
+            $(".status_info ul li").each(function () {
+                let titleText = $(this).children(".info_title").text();
+                let dataText = $(this).children(".info_num").text();
+                let changeTrendText = $(this).children(".info_variation").text();
+                
+                if (changeTrendText != "-")
+                    changeTrendText = `(🔺${changeTrendText}명)`;
+                else 
+                    changeTrendText = "";
+
+                coronaArray.push({name: titleText, value: `${dataText}명 ${changeTrendText}`});
             });
 
             editMsg.edit("데이터 조회 성공 ✅");
@@ -24,8 +31,7 @@ export function searchCoronaData (message, htmlparser, timeString) {
                 embed: {
                     color: 3447003,
                     title: "현재 코로나 현황",
-                    url: maplePersonSearchUrl,
-                    description: `현재시간 - ${timeString}`,
+                    description: $(".status_bottom > div > a").text(),
                     fields: coronaArray
                 }
             });
