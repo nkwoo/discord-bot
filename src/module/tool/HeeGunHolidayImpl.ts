60260ff3ef801c077516bb6c2acf1071f109cfdf
NKWOO <nkw9986@gmail.com>
2020-12-27 16:20:06 +0900

    [nkw] 1.8.2 2020-12-27
    1. 희건이 휴일 업데이트
    heekunholiday.json
    
    2. 휴일 조회 오류 수정
    HeeGunHolidayImpl.ts

--- src/module/tool/HeeGunHolidayImpl.ts
+++ src/module/tool/HeeGunHolidayImpl.ts
@@ -5,7 +5,7 @@ import heeKunHolidayJson from "../../../data/heekunholiday.json";
 import {Util} from "./interface/Util";
 import {UtilImpl} from "./UtilImpl";
 
-const DATE_PATTERN = /^(19|20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$/;
+const DATE_PATTERN = /^(19|20)\d{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[0-1])$/;
 const NUMBER_PATTERN = /^[0-9]*$/;
 
 export class HeeGunHolidayImpl implements HeeGunHoliday {
